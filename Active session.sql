f2909d754550214ab2ff958f21ff1d185382bb7a
eduardodba <57985139+eduardodba@users.noreply.github.com>
2019-11-27 03:29:47 -0300

    Update Active session.sql

--- Active session.sql
+++ Active session.sql
@@ -1,17 +1,16 @@
+--Verifica a quantidade de sessoes por login
 SELECT login_name, status, COUNT(*) as conexoes FROM sys.dm_exec_sessions 
 WHERE login_name <> 'CSFCPV\cluster_sql' and
 is_user_process = 1 GROUP BY login_name, status order by status
+
+
+--Verifica as sessoes ativas
 select login_name, status,host_name, program_name, * FROM sys.dm_exec_sessions WHERE 
 login_name <> 'sa'  and login_name <> 'CSFCPV\cluster_sql'
 and is_user_process = 1 order by 2 
 
 
-
-
-/*
-
-SESSOES COM LOGIN EM DETERMINADO HORARIO
-
+--sessoes ativas com login em determinado horario
 select login_name, status,host_name, program_name, login_time,* 
 FROM sys.dm_exec_sessions
 WHERE --login_name <> 'sa'  
@@ -20,6 +19,3 @@ WHERE --login_name <> 'sa'
 login_name='dmsql'
 and CONVERT(VARCHAR(25), login_time, 126) LIKE '%00:56:%'
 order by 2 
-
-
-*/
