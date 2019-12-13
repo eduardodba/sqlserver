c6bd54c3ff10564d225765fffe5cc20d6911d4d3
eduardodba <57985139+eduardodba@users.noreply.github.com>
2019-12-13 11:38:00 -0300

    Add files via upload

---
+++ move datafiles.sql
@@ -0,0 +1,27 @@
+--Para a replicação no nó secundário "SUSPEND DATA MOVIMENT...."
+--Readable Secondary = NO
+
+--Mover arquivos mdf e ldf para outro local
+USE MASTER
+GO
+ALTER DATABASE AdventureWorks2014   
+    MODIFY FILE ( NAME = AdventureWorks2014_Data,   
+                  FILENAME = 'E:\New_location\AdventureWorks2014_Data.mdf');  
+GO
+ 
+ALTER DATABASE AdventureWorks2014   
+    MODIFY FILE ( NAME = AdventureWorks2014_Log,   
+                  FILENAME = 'E:\New_location\AdventureWorks2014_Log.ldf');  
+GO
+
+--Ver atual dos arquivos
+SELECT name, physical_name AS NewLocation, state_desc AS OnlineStatus
+FROM sys.master_files  
+WHERE database_id = DB_ID(N'AdventureWorks2014')  
+GO
+
+--Stopa a Instância
+--Move os arquivos para o diretório apontado
+XCOPY "C:\SQL_DISKS\AGSQLHML_DATA\AdventureWorksDW2016\AdventureWorksDW2016_Data.mdf" "C:\SQL_DISKS\AGSQLHML_LOG\TESTE" /o /x /e /h /k
+--Starta a Instância
+
