5b278b7da0184db4f1d462ace5d504f7d336d991
eduardodba <57985139+eduardodba@users.noreply.github.com>
2020-01-21 01:56:45 -0300

    Update Fragmentacao_index.sql

--- Fragmentacao_index.sql
+++ Fragmentacao_index.sql
@@ -58,50 +58,6 @@ AND a.avg_fragmentation_in_percent >5
 AND d.[name]  IS NOT NULL
 ORDER BY a.avg_fragmentation_in_percent desc
 
---APLICANDO DDL PARA REBUILD OU REORGANIZE
-ALTER INDEX PK_EmployeePayHistory_BusinessEntityID_RateChangeDate ON HumanResources.EmployeePayHistory REBUILD
-ALTER INDEX PK_SalesPersonQuotaHistory_BusinessEntityID_QuotaDate ON Sales.SalesPersonQuotaHistory REBUILD
-ALTER INDEX AK_Product_ProductNumber ON Production.Product REBUILD
-ALTER INDEX AK_Product_rowguid ON Production.Product REBUILD
-ALTER INDEX AK_SpecialOfferProduct_rowguid ON Sales.SpecialOfferProduct REBUILD
-ALTER INDEX PK_StateProvince_StateProvinceID ON Person.StateProvince REBUILD
-ALTER INDEX IX_Store_SalesPersonID ON Sales.Store REBUILD
-ALTER INDEX PK_ProductProductPhoto_ProductID_ProductPhotoID ON Production.ProductProductPhoto REBUILD
-ALTER INDEX PK_ProductReview_ProductReviewID ON Production.ProductReview REBUILD
-ALTER INDEX IX_ProductVendor_UnitMeasureCode ON Purchasing.ProductVendor REBUILD
-ALTER INDEX PK_Vendor_BusinessEntityID ON Purchasing.Vendor REBUILD
-ALTER INDEX PK_CountryRegion_CountryRegionCode ON Person.CountryRegion REBUILD
-ALTER INDEX AK_CountryRegion_Name ON Person.CountryRegion REBUILD
-ALTER INDEX AK_Employee_NationalIDNumber ON HumanResources.Employee REBUILD
-ALTER INDEX PK_EmployeeDepartmentHistory_BusinessEntityID_StartDate_DepartmentID ON HumanResources.EmployeeDepartmentHistory REBUILD
-ALTER INDEX PK_ProductListPriceHistory_ProductID_StartDate ON Production.ProductListPriceHistory REBUILD
-ALTER INDEX PK_SpecialOfferProduct_SpecialOfferID_ProductID ON Sales.SpecialOfferProduct REBUILD
-ALTER INDEX PK_ProductCostHistory_ProductID_StartDate ON Production.ProductCostHistory REBUILD
-ALTER INDEX AK_ProductDescription_rowguid ON Production.ProductDescription REBUILD
-ALTER INDEX IX_ProductReview_ProductID_Name ON Production.ProductReview REBUILD
-ALTER INDEX AK_Store_rowguid ON Sales.Store REBUILD
-ALTER INDEX AK_Employee_LoginID ON HumanResources.Employee REBUILD
-ALTER INDEX IX_BusinessEntityContact_PersonID ON Person.BusinessEntityContact REBUILD
-ALTER INDEX IX_BusinessEntityContact_ContactTypeID ON Person.BusinessEntityContact REBUILD
-ALTER INDEX AK_BusinessEntityContact_rowguid ON Person.BusinessEntityContact REORGANIZE
-ALTER INDEX AK_Product_Name ON Production.Product REORGANIZE
-ALTER INDEX PK_DatabaseLog_DatabaseLogID ON dbo.DatabaseLog REORGANIZE
-ALTER INDEX PK_ProductModelProductDescriptionCulture_ProductModelID_ProductDescriptionID_CultureID ON Production.ProductModelProductDescriptionCulture REORGANIZE
-ALTER INDEX PK_ProductVendor_ProductID_BusinessEntityID ON Purchasing.ProductVendor REORGANIZE
-ALTER INDEX PK_BusinessEntityContact_BusinessEntityID_PersonID_ContactTypeID ON Person.BusinessEntityContact REORGANIZE
-ALTER INDEX PK_ProductInventory_ProductID_LocationID ON Production.ProductInventory REORGANIZE
-ALTER INDEX IX_PurchaseOrderHeader_VendorID ON Purchasing.PurchaseOrderHeader REORGANIZE
-ALTER INDEX IX_PurchaseOrderHeader_EmployeeID ON Purchasing.PurchaseOrderHeader REORGANIZE
-ALTER INDEX IX_WorkOrder_ProductID ON Production.WorkOrder REORGANIZE
-ALTER INDEX PK_BillOfMaterials_BillOfMaterialsID ON Production.BillOfMaterials REORGANIZE
-ALTER INDEX IX_WorkOrderRouting_ProductID ON Production.WorkOrderRouting REORGANIZE
-ALTER INDEX IX_BillOfMaterials_UnitMeasureCode ON Production.BillOfMaterials REORGANIZE
-ALTER INDEX IX_TransactionHistoryArchive_ProductID ON Production.TransactionHistoryArchive REORGANIZE
-ALTER INDEX PK_JobCandidate_JobCandidateID ON HumanResources.JobCandidate REORGANIZE
-ALTER INDEX PK_Product_ProductID ON Production.Product REORGANIZE
-ALTER INDEX IX_TransactionHistory_ProductID ON Production.TransactionHistory REORGANIZE
-ALTER INDEX IX_Person_LastName_FirstName_MiddleName ON Person.Person REORGANIZE
-ALTER INDEX IX_TransactionHistoryArchive_ReferenceOrderID_ReferenceOrderLineID ON Production.TransactionHistoryArchive REORGANIZE
 --GERANDO INFORMAÇÃO DA FRAGMENTAÇÃO APÓS REBUILD OU REORGANIZE
 
 --INSERINDO EM TABELA TEMPORARIO
@@ -144,4 +100,46 @@ AND A.indice=B.indice
 
 --ELIMINANDO A TABELA TEMP
 DROP TABLE #ANALISE_IX
+
+
+
+
+
+--Listar todos index fragmentados de uma database
+SELECT dbschemas.[name] as 'Schema',
+dbtables.[name] as 'Table',
+dbindexes.[name] as 'Index',
+indexstats.avg_fragmentation_in_percent,
+indexstats.page_count
+FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS indexstats
+INNER JOIN sys.tables dbtables on dbtables.[object_id] = indexstats.[object_id]
+INNER JOIN sys.schemas dbschemas on dbtables.[schema_id] = dbschemas.[schema_id]
+INNER JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
+AND indexstats.index_id = dbindexes.index_id
+WHERE indexstats.database_id = DB_ID()
+ORDER BY indexstats.avg_fragmentation_in_percent desc
+
+-- adicionar clausula para buscar uma tabela em específico
+-- WHERE indexstats.database_id = DB_ID() AND dbtables.[ name ] like '%%' 
+
+
+
+--reorganizar um índice específico
+ALTER INDEX IX_NAME
+  ON dbo.Employee  
+REORGANIZE ;   
+GO  
+
+-- reorganizar todos os índices de uma tabela
+ALTER INDEX ALL ON dbo.Employee  
+REORGANIZE ;   
+GO  
+
+-- rebuild de um índice específico
+ALTER INDEX PK_Employee_BusinessEntityID ON dbo.Employee
+REBUILD;
+
+-- rebuild de todos índice de uma tabela
+ALTER INDEX ALL ON dbo.Employee
+REBUILD;
 
