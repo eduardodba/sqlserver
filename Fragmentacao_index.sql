2bbe8f86aff09355a7425694d35e982ad04d0a05
eduardodba <57985139+eduardodba@users.noreply.github.com>
2020-02-09 09:43:56 -0300

    Update Fragmentacao_index.sql

--- Fragmentacao_index.sql
+++ Fragmentacao_index.sql
@@ -1,126 +1,54 @@
---DOWNLOAD ADVENTURE WORKS
---https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2017.bak
---REFERENCIAS
---FRAGMENTAÇÃO ENTRE 5 E 30 APLICAR REORGANIZE
---FRAGMENTAÇÃO MAIOR QUE 30 APLICAR REBUILD
---REORGANIZAR INDICES
-USE AdventureWorks2017
-SELECT '1' TEMPO,
-c.[name] as 'Schema',
-b.[name] as 'Tabela',
-d.[name] as 'Indice',
-a.avg_fragmentation_in_percent as PCT_Frag,
-a.page_count as 'Paginas'
---INSERINDO EM TABELA TEMPORARIO
-INTO #ANALISE_IX
+--PARA UMA DATABASE
+USE ADVENTUREWORKS2017
+GO
+IF EXISTS (SELECT * FROM tempdb.sys.all_objects WHERE name LIKE '#bbc%' )
+DROP TABLE #bbc
+CREATE TABLE #bbc (DatabaseName VARCHAR(100),ObjectName VARCHAR(100),Index_id int, indexName VARCHAR(100),avg_fragmentation_percent FLOAT,IndexType VARCHAR(100),Action_Required VARCHAR(100) DEFAULT 'NA')
+GO
+INSERT INTO #bbc (DatabaseName,ObjectName,Index_id, indexName,avg_fragmentation_percent,IndexType)
+SELECT DB_NAME() AS DatabaseName, OBJECT_NAME (a.OBJECT_ID) AS ObjectName, 
+	   a.index_id, b.name AS IndexName, 
+	   avg_fragmentation_in_percent, index_type_desc
 FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS a
-INNER JOIN sys.tables b 
-     on b.[object_id] = a.[object_id]
-INNER JOIN sys.schemas c 
-     on b.[schema_id] = c.[schema_id]
-INNER JOIN sys.indexes AS d 
-     ON d.[object_id] = a.[object_id]
-	AND a.index_id = d.index_id
-WHERE a.database_id = DB_ID()
-AND a.avg_fragmentation_in_percent >5
-AND d.[name]  IS NOT NULL
-ORDER BY a.avg_fragmentation_in_percent desc
-
---VERIFICANDO DADOS DA TEMPO
-SELECT * FROM #ANALISE_IX
-
-
---GERANDO DDL PARA RECONSTRUIR OU REORGANIZAR INDICES
-SELECT 
-c.[name] as 'Schema',
-b.[name] as 'Tabela',
-d.[name] as 'Index',
-a.avg_fragmentation_in_percent as PCT_Frag,
-a.page_count as 'Paginas',
-
---GERADNDO DDL
-CASE WHEN a.avg_fragmentation_in_percent >5 
-          and a.avg_fragmentation_in_percent<30
-THEN  'ALTER INDEX '+d.[name]+' ON '+c.[name]+'.'+b.[name]+' REORGANIZE' 
-   WHEN a.avg_fragmentation_in_percent >=30 
- THEN 'ALTER INDEX '+d.[name]+' ON '+c.[name]+'.'+b.[name]+' REBUILD' 
- ELSE ' ' END COMANDO
-FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS a
-INNER JOIN sys.tables b 
-     on b.[object_id] = a.[object_id]
-INNER JOIN sys.schemas c 
-     on b.[schema_id] = c.[schema_id]
-INNER JOIN sys.indexes AS d 
-     ON d.[object_id] = a.[object_id]
-	AND a.index_id = d.index_id
-WHERE a.database_id = DB_ID()
-AND a.avg_fragmentation_in_percent >5
-AND d.[name]  IS NOT NULL
-ORDER BY a.avg_fragmentation_in_percent desc
-
---GERANDO INFORMAÇÃO DA FRAGMENTAÇÃO APÓS REBUILD OU REORGANIZE
-
---INSERINDO EM TABELA TEMPORARIO
---DROP TABLE #ANALISE_IX
-INSERT INTO #ANALISE_IX
-SELECT '2' AS TEMPO,
-c.[name] as 'Schema',
-b.[name] as 'Tabela',
-d.[name] as 'indice',
-a.avg_fragmentation_in_percent as PCT_Frag,
-a.page_count as 'Paginas'
-
-FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS a
-INNER JOIN sys.tables b 
-     on b.[object_id] = a.[object_id]
-INNER JOIN sys.schemas c 
-     on b.[schema_id] = c.[schema_id]
-INNER JOIN sys.indexes AS d 
-     ON d.[object_id] = a.[object_id]
-	AND a.index_id = d.index_id
-WHERE a.database_id = DB_ID()
-AND a.avg_fragmentation_in_percent >5
-AND d.[name]  IS NOT NULL
-ORDER BY a.avg_fragmentation_in_percent desc
-
---COMPARANDO ANTES E DEPOIS
---SELECT * FROM #ANALISE_IX
-WITH ANTES (tempo, tabela, indice,pct)
-as (select a.tempo,a.tabela,a.indice, a.Pct_frag from #ANALISE_IX a
-		where a.tempo='1'),
-DEPOIS (tempo, tabela, indice,pct)
-as (select a.tempo,a.tabela,a.indice, a.Pct_frag from #ANALISE_IX a
-		where a.tempo='2')
-
-SELECT A.TABELA, A.indice,A.PCT,B.PCT,ISNULL((B.PCT/A.PCT),100) AS REDUCAO
-FROM ANTES A
-LEFT JOIN DEPOIS B
-ON A.TABELA=B.TABELA
-AND A.indice=B.indice
-
---ELIMINANDO A TABELA TEMP
-DROP TABLE #ANALISE_IX
+JOIN sys.indexes AS b 
+ON a.OBJECT_ID = b.OBJECT_ID AND a.index_id = b.index_id
+WHERE b.index_id <> 0 AND avg_fragmentation_in_percent <>0
+GO 
+UPDATE #bbc SET Action_Required ='Rebuild' WHERE avg_fragmentation_percent >30 
+GO
+UPDATE #bbc SET Action_Required ='Rorganize' WHERE avg_fragmentation_percent <30 AND avg_fragmentation_percent >5
+GO
+SELECT * FROM #bbc ORDER BY avg_fragmentation_percent DESC
 
 
 
-
-
---Listar todos index fragmentados de uma database
-SELECT dbschemas.[name] as 'Schema',
-dbtables.[name] as 'Table',
-dbindexes.[name] as 'Index',
-indexstats.avg_fragmentation_in_percent,
-indexstats.page_count
-FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS indexstats
-INNER JOIN sys.tables dbtables on dbtables.[object_id] = indexstats.[object_id]
-INNER JOIN sys.schemas dbschemas on dbtables.[schema_id] = dbschemas.[schema_id]
-INNER JOIN sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
-AND indexstats.index_id = dbindexes.index_id
-WHERE indexstats.database_id = DB_ID()
-ORDER BY indexstats.avg_fragmentation_in_percent desc
-
--- adicionar clausula para buscar uma tabela em específico
--- WHERE indexstats.database_id = DB_ID() AND dbtables.[ name ] like '%%' 
+/* PARA TODAS AS BASES DA INSTÂNCIA
+If exists (select * from tempdb.sys.all_objects where name like '#bbc%' )
+drop table #bbc
+create table #bbc
+(DatabaseName varchar(100),ObjectName varchar(100),Index_id int, indexName varchar(100),avg_fragmentation_percent float,IndexType varchar(100),Action_Required varchar(100) default 'NA')
+go
+insert into #bbc (DatabaseName,ObjectName,Index_id, indexName,avg_fragmentation_percent,IndexType)
+exec master.sys.sp_MSforeachdb ' USE [?]
+SELECT db_name() as DatabaseName, OBJECT_NAME (a.object_id) as ObjectName, 
+a.index_id, b.name as IndexName, 
+avg_fragmentation_in_percent, index_type_desc
+-- , record_count, avg_page_space_used_in_percent --(null in limited)
+FROM sys.dm_db_index_physical_stats (db_id(), NULL, NULL, NULL, NULL) AS a
+JOIN sys.indexes AS b 
+ON a.object_id = b.object_id AND a.index_id = b.index_id
+WHERE b.index_id <> 0 and avg_fragmentation_in_percent <>0'
+go 
+update #bbc
+set Action_Required ='Rebuild'
+where avg_fragmentation_percent >30 
+go
+update #bbc
+set Action_Required ='Rorganize'
+where avg_fragmentation_percent <30 and avg_fragmentation_percent >5
+go
+select * from #bbc
+*/
 
 
 
