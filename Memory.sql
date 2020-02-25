4dbb106770a98addabb54d75cca80242290e1bd6
eduardodba <57985139+eduardodba@users.noreply.github.com>
2020-02-25 04:55:39 -0300

    Add files via upload

---
+++ Memory.sql
@@ -0,0 +1,192 @@
+--analise Memory Grants Outstanding / Memory Grants Pending                                                                                                                                                                                  
+SELECT object_name, counter_name, cntr_value
+FROM sys.dm_os_performance_counters
+WHERE [object_name] LIKE '%Memory Manager%'
+AND [counter_name] LIKE 'Memory Grants%'
+
+
+--identifica os MemObje que mais consomem memória
+select * 
+from sys.dm_os_memory_objects mo join 
+     sys.dm_os_memory_clerks mc 
+on mo.page_allocator_address = mc.page_allocator_address 
+where mc.type = 'MEMORYCLERK_SQLGENERAL' 
+order by pages_in_bytes desc
+
+
+-- Script to for Examining Buffer Pool Usage demo
+SELECT
+	COUNT (*) * 8 / 1024 AS [MBUsed],
+	SUM ([free_space_in_bytes]) / (1024 * 1024) AS [MBEmpty]
+FROM sys.dm_os_buffer_descriptors;
+GO
+
+
+-- And by database
+SELECT 
+	(CASE WHEN ([database_id] = 32767)
+		THEN N'Resource Database'
+		ELSE DB_NAME ([database_id]) END) AS [DatabaseName],
+	COUNT (*) * 8 / 1024 AS [MBUsed],
+	SUM ([free_space_in_bytes]) / (1024 * 1024) AS [MBEmpty]
+FROM sys.dm_os_buffer_descriptors
+GROUP BY [database_id];
+GO
+
+
+
+/**********************************************************
+*   top procedures consumo de memória por execução
+*   (this will show mostly reports &amp; jobs)
+***********************************************************/
+SELECT TOP 100 *
+FROM
+(
+    SELECT
+         DatabaseName       = DB_NAME(qt.dbid)
+        ,ObjectName         = OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid)
+        ,DiskReads          = SUM(qs.total_physical_reads)   -- The worst reads, disk reads
+        ,MemoryReads        = SUM(qs.total_logical_reads)    --Logical Reads are memory reads
+        ,Executions         = SUM(qs.execution_count)
+        ,IO_Per_Execution   = SUM((qs.total_physical_reads + qs.total_logical_reads) / qs.execution_count)
+        ,CPUTime            = SUM(qs.total_worker_time)
+        ,DiskWaitAndCPUTime = SUM(qs.total_elapsed_time)
+        ,MemoryWrites       = SUM(qs.max_logical_writes)
+        ,DateLastExecuted   = MAX(qs.last_execution_time)
+       
+    FROM sys.dm_exec_query_stats AS qs
+    CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
+    GROUP BY DB_NAME(qt.dbid), OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid)
+
+) T
+ORDER BY IO_Per_Execution DESC
+
+
+
+
+/**********************************************************
+*   top procedures consumo total de memória
+*   (this will show more operational procedures)
+***********************************************************/
+SELECT TOP 100 *
+FROM
+(
+    SELECT
+         DatabaseName       = DB_NAME(qt.dbid)
+        ,ObjectName         = OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid)
+        ,DiskReads          = SUM(qs.total_physical_reads)   -- The worst reads, disk reads
+        ,MemoryReads        = SUM(qs.total_logical_reads)    --Logical Reads are memory reads
+        ,Total_IO_Reads     = SUM(qs.total_physical_reads + qs.total_logical_reads)
+        ,Executions         = SUM(qs.execution_count)
+        ,IO_Per_Execution   = SUM((qs.total_physical_reads + qs.total_logical_reads) / qs.execution_count)
+        ,CPUTime            = SUM(qs.total_worker_time)
+        ,DiskWaitAndCPUTime = SUM(qs.total_elapsed_time)
+        ,MemoryWrites       = SUM(qs.max_logical_writes)
+        ,DateLastExecuted   = MAX(qs.last_execution_time)
+       
+    FROM sys.dm_exec_query_stats AS qs
+    CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
+    GROUP BY DB_NAME(qt.dbid), OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid)
+) T
+ORDER BY Total_IO_Reads DESC
+
+
+
+
+/**********************************************************
+*   top adhoc queries memory consumption total
+***********************************************************/
+SELECT TOP 100 *
+FROM
+(
+    SELECT
+         DatabaseName       = DB_NAME(qt.dbid)
+        ,QueryText          = qt.text      
+        ,DiskReads          = SUM(qs.total_physical_reads)   -- The worst reads, disk reads
+        ,MemoryReads        = SUM(qs.total_logical_reads)    --Logical Reads are memory reads
+        ,Total_IO_Reads     = SUM(qs.total_physical_reads + qs.total_logical_reads)
+        ,Executions         = SUM(qs.execution_count)
+        ,IO_Per_Execution   = SUM((qs.total_physical_reads + qs.total_logical_reads) / qs.execution_count)
+        ,CPUTime            = SUM(qs.total_worker_time)
+        ,DiskWaitAndCPUTime = SUM(qs.total_elapsed_time)
+        ,MemoryWrites       = SUM(qs.max_logical_writes)
+        ,DateLastExecuted   = MAX(qs.last_execution_time)
+       
+    FROM sys.dm_exec_query_stats AS qs
+    CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
+    WHERE OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid) IS NULL
+    GROUP BY DB_NAME(qt.dbid), qt.text, OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid)
+) T
+ORDER BY Total_IO_Reads DESC
+
+
+
+/**********************************************************
+*   top adhoc queries memory consumption per execution
+***********************************************************/
+SELECT TOP 100 *
+FROM
+(
+    SELECT
+         DatabaseName       = DB_NAME(qt.dbid)
+        ,QueryText          = qt.text      
+        ,DiskReads          = SUM(qs.total_physical_reads)   -- The worst reads, disk reads
+        ,MemoryReads        = SUM(qs.total_logical_reads)    --Logical Reads are memory reads
+        ,Total_IO_Reads     = SUM(qs.total_physical_reads + qs.total_logical_reads)
+        ,Executions         = SUM(qs.execution_count)
+        ,IO_Per_Execution   = SUM((qs.total_physical_reads + qs.total_logical_reads) / qs.execution_count)
+        ,CPUTime            = SUM(qs.total_worker_time)
+        ,DiskWaitAndCPUTime = SUM(qs.total_elapsed_time)
+        ,MemoryWrites       = SUM(qs.max_logical_writes)
+        ,DateLastExecuted   = MAX(qs.last_execution_time)
+       
+    FROM sys.dm_exec_query_stats AS qs
+    CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
+    WHERE OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid) IS NULL
+    GROUP BY DB_NAME(qt.dbid), qt.text, OBJECT_SCHEMA_NAME(qt.objectid,dbid) + '.' + OBJECT_NAME(qt.objectid, qt.dbid)
+) T
+ORDER BY IO_Per_Execution DESC
+
+
+
+/*************************************************************
+*Extrair informações sobre a condição da memória do SO e da memória SQL
+**************************************************************/
+SELECT CONVERT (varchar(30), GETDATE(), 121) as [RunTime],
+dateadd (ms, (rbf.[timestamp] - tme.ms_ticks), GETDATE()) as [Notification_Time],
+cast(record as xml).value('(//Record/ResourceMonitor/Notification)[1]', 'varchar(30)') AS [Notification_type],
+cast(record as xml).value('(//Record/MemoryRecord/MemoryUtilization)[1]', 'bigint') AS [MemoryUtilization %],
+cast(record as xml).value('(//Record/MemoryNode/@id)[1]', 'bigint') AS [Node Id],
+cast(record as xml).value('(//Record/ResourceMonitor/IndicatorsProcess)[1]', 'int') AS [Process_Indicator],
+cast(record as xml).value('(//Record/ResourceMonitor/IndicatorsSystem)[1]', 'int') AS [System_Indicator],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect/@type)[1]', 'varchar(30)') AS [type],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect/@state)[1]', 'varchar(30)') AS [state],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect/@reversed)[1]', 'int') AS [reserved],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect)[1]', 'bigint') AS [Effect],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect[2]/@type)[1]', 'varchar(30)') AS [type],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect[2]/@state)[1]', 'varchar(30)') AS [state],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect[2]/@reversed)[1]', 'int') AS [reserved],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect)[2]', 'bigint') AS [Effect],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect[3]/@type)[1]', 'varchar(30)') AS [type],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect[3]/@state)[1]', 'varchar(30)') AS [state],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect[3]/@reversed)[1]', 'int') AS [reserved],
+cast(record as xml).value('(//Record/ResourceMonitor/Effect)[3]', 'bigint') AS [Effect],
+cast(record as xml).value('(//Record/MemoryNode/ReservedMemory)[1]', 'bigint') AS [SQL_ReservedMemory_KB],
+cast(record as xml).value('(//Record/MemoryNode/CommittedMemory)[1]', 'bigint') AS [SQL_CommittedMemory_KB],
+cast(record as xml).value('(//Record/MemoryNode/AWEMemory)[1]', 'bigint') AS [SQL_AWEMemory],
+cast(record as xml).value('(//Record/MemoryNode/SinglePagesMemory)[1]', 'bigint') AS [SinglePagesMemory],
+cast(record as xml).value('(//Record/MemoryNode/MultiplePagesMemory)[1]', 'bigint') AS [MultiplePagesMemory],
+cast(record as xml).value('(//Record/MemoryRecord/TotalPhysicalMemory)[1]', 'bigint') AS [TotalPhysicalMemory_KB],
+cast(record as xml).value('(//Record/MemoryRecord/AvailablePhysicalMemory)[1]', 'bigint') AS [AvailablePhysicalMemory_KB],
+cast(record as xml).value('(//Record/MemoryRecord/TotalPageFile)[1]', 'bigint') AS [TotalPageFile_KB],
+cast(record as xml).value('(//Record/MemoryRecord/AvailablePageFile)[1]', 'bigint') AS [AvailablePageFile_KB],
+cast(record as xml).value('(//Record/MemoryRecord/TotalVirtualAddressSpace)[1]', 'bigint') AS [TotalVirtualAddressSpace_KB],
+cast(record as xml).value('(//Record/MemoryRecord/AvailableVirtualAddressSpace)[1]', 'bigint') AS [AvailableVirtualAddressSpace_KB],
+cast(record as xml).value('(//Record/@id)[1]', 'bigint') AS [Record Id],
+cast(record as xml).value('(//Record/@type)[1]', 'varchar(30)') AS [Type],
+cast(record as xml).value('(//Record/@time)[1]', 'bigint') AS [Record Time],
+tme.ms_ticks as [Current Time]
+FROM sys.dm_os_ring_buffers rbf
+cross join sys.dm_os_sys_info tme
+where rbf.ring_buffer_type = 'RING_BUFFER_RESOURCE_MONITOR' --and cast(record as xml).value('(//Record/ResourceMonitor/Notification)[1]', 'varchar(30)') = 'RESOURCE_MEMPHYSICAL_LOW'
+ORDER BY rbf.timestamp ASC
