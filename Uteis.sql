77bd330a455ebd02655f71cc7b5599d0729b6fc8
eduardodba <57985139+eduardodba@users.noreply.github.com>
2021-07-09 17:40:28 -0300

    Update Uteis.sql

--- Uteis.sql
+++ Uteis.sql
@@ -178,3 +178,24 @@ from sys.dm_sql_referenced_entities('sysfunc.FI_SP_AUTWEB_LEVANTAREFINCOMPLETO_0
 
 --Subir base em Recovery
 restore database BASE_NAME with recovery
+
+
+
+--SELECT EM TABELA PARTICIONADA
+
+
+SELECT 
+    p.partition_number AS [Partition], 
+    fg.name AS [Filegroup], 
+    p.Rows
+FROM sys.partitions p
+    INNER JOIN sys.allocation_units au
+    ON au.container_id = p.hobt_id
+    INNER JOIN sys.filegroups fg
+    ON fg.data_space_id = au.data_space_id
+WHERE p.object_id = OBJECT_ID('partitionTable')
+ORDER BY [Partition];
+
+
+
+select count(*), $partition.PF_ANO(ano) [Partition Number], ano FROM [partitionTable] group by $partition.PF_ANO(ano),ano order by 2
