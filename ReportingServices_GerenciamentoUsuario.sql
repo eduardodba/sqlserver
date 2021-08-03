73517baccd13d26efc72672fcc3b73471b6d401c
eduardodba <57985139+eduardodba@users.noreply.github.com>
2021-08-02 21:18:18 -0300

    Update ReportingServices_GerenciamentoUsuario.sql

--- ReportingServices_GerenciamentoUsuario.sql
+++ ReportingServices_GerenciamentoUsuario.sql
@@ -276,16 +276,15 @@ ALTER PROCEDURE secmonit.sp_ListaPermissoes @user varchar(100) AS
 		RoleName sysname,  
 		MemberName sysname
 	)  
-	EXEC dbo.sp_MSforeachdb 'insert into #RolesMembers select ''[?]'', ''['' + r.name + '']'', ''['' + m.name + '']''  
+	EXEC dbo.sp_MSforeachdb 'insert into #RolesMembers select ''[?]'', r.name ,  m.name   
 								FROM [?].sys.database_role_members rm  
 								INNER JOIN [?].sys.database_principals r ON rm.role_principal_id = r.principal_id  
 								INNER JOIN [?].sys.database_principals m ON rm.member_principal_id = m.principal_id  
 								INNER JOIN [?].sys.database_permissions e ON e.grantee_principal_id=m.principal_id';  
 	SELECT * FROM #RolesMembers  
-	WHERE MemberName like '%'+@user+'%'  
+    WHERE MemberName = @user
 	ORDER BY [Database], [RoleName]
 GO
-
 
 
 
